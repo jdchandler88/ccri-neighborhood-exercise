698f4a1127f71e2eb0bca36370128d04b989c27e
Darin Chandler <jdchandler88@gmail.com>
2020-06-12 16:16:59 -0400

    GREEN TDD phase (for test 1, and luckily, test 3). this implementation passes disjoint neighborhood tests (see tests 1 and 3). this does not take into account edges and multiple counts

--- src/main/java/ccri/neighborhood/exercise/CellCounter.java
+++ src/main/java/ccri/neighborhood/exercise/CellCounter.java
@@ -17,7 +17,34 @@ public final class CellCounter {
      * @return number of neighbors within 'neighborDistance' of cells with positive value
      */
     public static int count(int[][] neighborhood, int neighborDistance) {
-        return -1;
+
+        int count = 0;
+
+        //search for positive number
+        for (int x = 0; x < neighborhood.length; x++) {
+            for (int y = 0; y < neighborhood[0].length; y++) {
+
+                if (neighborhood[x][y] > 0) {
+                    //count number of neighbors within distance
+                    for (int xSearch = (x-neighborDistance); xSearch <= x+neighborDistance; xSearch++) {
+                        for (int ySearch = (y-neighborDistance); ySearch <= y+neighborDistance; ySearch++) {
+
+                            //make sure that the cell is within manhattan distance
+                            int xDist = Math.abs(xSearch - x);
+                            int yDist = Math.abs(ySearch - y);
+
+                            if (xDist + yDist <= neighborDistance) {
+                                count++;
+                            }
+                        }
+                    }
+
+                }
+
+            }
+        }
+
+        return count;
     }
 
 }
